640a327def9e16d2a62ff2340517ea942e15f202
Zhengyu-Li <32192207+Zhengyu-Li@users.noreply.github.com>
2019-06-17 17:13:07 +0800

    add MNIST dataset

--- teacher_student/data_providers/utils.py
+++ teacher_student/data_providers/utils.py
@@ -1,7 +1,7 @@
 from .cifar import Cifar10DataProvider, Cifar100DataProvider, \
     Cifar10AugmentedDataProvider, Cifar100AugmentedDataProvider
-from .cifar2 import Cifar100DataProvider2
 from .svhn import SVHNDataProvider
+from .mnist import MnistDataProvider
 
 
 def get_data_provider_by_name(name, train_params):
@@ -11,13 +11,13 @@ def get_data_provider_by_name(name, train_params):
     if name == 'C10+':
         return Cifar10AugmentedDataProvider(**train_params)
     if name == 'C100':
-        return Cifar100DataProvider2(**train_params)
-    if name == 'C100o':
         return Cifar100DataProvider(**train_params)
     if name == 'C100+':
         return Cifar100AugmentedDataProvider(**train_params)
     if name == 'SVHN':
         return SVHNDataProvider(**train_params)
+    if name == 'MNIST':
+        return MnistDataProvider(**train_params)
     else:
         print("Sorry, data provider for `%s` dataset "
               "was not implemented yet" % name)
